40862dbb941d914129337e2a1a3be40237368a6e
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-03-08 22:46:14 -0300

    chore: add Profile component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import { ExperienceBar } from '../components/ExperienceBar';
+import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
@@ -11,6 +12,12 @@ export default function Home() {
       </Head>
 
       <ExperienceBar />
+
+      <section>
+        <div className={styles.cycleContainer}>
+          <Profile />
+        </div>
+      </section>
     </main>
   );
 }
