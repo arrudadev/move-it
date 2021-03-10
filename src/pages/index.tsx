38a319ba28f2a1f6bd74ae6b3f4caa6708a93373
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-03-09 23:41:47 -0300

    chore: add Countdown component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
 import { ExperienceBar } from '../components/ExperienceBar';
 import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
@@ -18,6 +19,7 @@ export default function Home() {
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
       </section>
     </main>
