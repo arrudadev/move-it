1d571e31b755241feacdc337d04a67ecd4d1b85b
Alexandre <alexandre.monteiro.bec@gmail.com>
2021-03-10 21:59:07 -0300

    chore: getting ExperienceBar info from context

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -16,6 +16,7 @@ interface ChallengesContextData {
   level: number;
   currentExperience: number;
   challengesCompleted: number;
+  experienceToNextLevel: number;
   activeChallenge: Challenge;
   levelUp: () => void;
   startNewChallenge: () => void;
@@ -25,10 +26,13 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
   const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
+  const [currentExperience, setCurrentExperience] = useState(25);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
   const [activeChallenge, setActiveChallenge] = useState(null);
+
+  // eslint-disable-next-line no-restricted-properties
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2);
 
   function levelUp() {
     setLevel(level + 1);
@@ -47,6 +51,7 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         level,
         currentExperience,
         challengesCompleted,
+        experienceToNextLevel,
         activeChallenge,
         levelUp,
         startNewChallenge,
